12b1c1ec5c9f949bf9bcf0cad04553e9fb33e5e2
Anne Marie Johnston <annemarie.johnston@outlook.com>
2018-08-15 12:18:33 +0000

    updated wsgi.py

--- django_todo/wsgi.py
+++ django_todo/wsgi.py
@@ -10,10 +10,19 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-# That's the default
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django-todo.settings")
 
-# That's recommended by https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+
+# 1. That's the default
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django-todo.settings")
+
+
+# 2. That's recommended by https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 # os.environ["DJANGO_SETTINGS_MODULE"] = "django_todo.settings"
+
+
+# 3. Found on StackOverflow
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config")
+os.environ.setdefault("DJANGO_CONFIGURATION", "Production")
+
 
 application = get_wsgi_application()
